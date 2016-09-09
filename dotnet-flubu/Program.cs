41716f3e84ab0daf4dbf2951e7c3f4b31dc662be
zoroz <zoro.zorec@gmail.com>
2016-09-09 22:45:27 +0200

    experimenting with command line parameter parsing

--- dotnet-flubu/Program.cs
+++ dotnet-flubu/Program.cs
@@ -1,12 +1,77 @@
-﻿using System;
+﻿using Microsoft.DotNet.Cli.Utils;
+using Microsoft.DotNet.ProjectModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
-namespace ConsoleApplication
+namespace flubu
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            var parameters = new DotnetCommandParams();
+            DotnetCommandParams parameters = new DotnetCommandParams();
+
+            parameters.Parse(args);
+
+            if (parameters.Help)
+            {
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(parameters.Command))
+            {
+                Reporter.Error.WriteLine("Missing argument <COMMAND>");
+                parameters.ShowHint();
+                return 1;
+            }
+
+            if (parameters.ParentProcessId.HasValue)
+            {
+                RegisterForParentProcessExit(parameters.ParentProcessId.Value);
+            }
+
+            IEnumerable<ProjectContext> projectContexts = CreateProjectContexts(parameters.ProjectPath);
+            ProjectContext projectContext = parameters.Framework != null
+                ? projectContexts.First(p => p.TargetFramework == parameters.Framework)
+                : projectContexts.First();
+
+            var runner = new CommandRunner(projectContext);
+
+            return runner.Run(parameters);
+        }
+
+        private static IEnumerable<ProjectContext> CreateProjectContexts(string projectPath)
+        {
+            projectPath = projectPath ?? Directory.GetCurrentDirectory();
+
+            if (!projectPath.EndsWith(Project.FileName, StringComparison.Ordinal))
+            {
+                projectPath = Path.Combine(projectPath, Project.FileName);
+            }
+
+            if (!File.Exists(projectPath))
+            {
+                throw new InvalidOperationException($"{projectPath} does not exist.");
+            }
+
+            return ProjectContext.CreateContextForEachFramework(projectPath);
+        }
+
+        private static void RegisterForParentProcessExit(int id)
+        {
+            Process parentProcess = Process.GetProcesses().FirstOrDefault(p => p.Id == id);
+
+            if (parentProcess != null)
+            {
+                parentProcess.EnableRaisingEvents = true;
+                parentProcess.Exited += (sender, eventArgs) =>
+                {
+                    Process.GetCurrentProcess().Kill();
+                };
+            }
         }
     }
 }
