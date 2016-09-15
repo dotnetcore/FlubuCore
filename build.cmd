cba3b2250de7e17d2e2eff269806093794ce7c42
zoroz <zoro.zorec@gmail.com>
2016-09-15 20:56:27 +0200

    added simple build script for flubu compile

---
+++ build.cmd
@@ -0,0 +1,16 @@
+cd flubu.core
+dotnet restore
+dotnet pack
+cd ..
+
+cd dotnet-flubu
+dotnet restore
+dotnet pack
+
+cd ..
+copy flubu.core\bin\Debug\*.nupkg flubu.console\nuget
+copy dotnet-flubu\bin\Debug\*.nupkg flubu.console\nuget
+
+cd flubu.console
+dotnet restore -f nuget
+cd ..
