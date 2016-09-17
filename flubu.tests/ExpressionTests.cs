4a8d827a3da1b99ed30c9f0f88a0c5601ec8f257
zoroz <zoro.zorec@gmail.com>
2016-09-17 18:28:14 +0200

    Added sample expression methods for dependson

---
+++ flubu.tests/ExpressionTests.cs
@@ -0,0 +1,123 @@
+﻿using flubu.Targeting;
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using flubu.Tasks;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace flubu.tests
+{
+    public static class ExpressionTests
+    {
+        public static MemberInfo GetMemberInfo(this Expression expression)
+        {
+            var lambda = (LambdaExpression)expression;
+
+            MemberExpression memberExpression;
+
+            if (lambda.Body is UnaryExpression)
+            {
+                var unaryExpression = (UnaryExpression)lambda.Body;
+
+                memberExpression = (MemberExpression)unaryExpression.Operand;
+            }
+            else
+            {
+                memberExpression = (MemberExpression)lambda.Body;
+            }
+
+            return memberExpression.Member;
+        }
+
+        public static string DependsOn<TProp>(this ITarget target, Expression<Func<TProp>> t)
+        {
+            return t.GetMemberInfo().Name;
+        }
+
+        public static void SampleCall()
+        {
+            ITarget t = new SimpleTarget();
+            ITarget t1 = new SimpleTarget();
+            t.DependsOn(() => t1);
+        }
+    }
+
+    internal class SimpleTarget : ITarget
+    {
+        public ICollection<string> Dependencies
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsHidden
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string TargetName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Stopwatch TaskStopwatch
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ITarget DependsOn(params string[] targetNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITarget Do(Action<ITaskContext> targetAction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(ITaskContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITarget OverrideDo(Action<ITaskContext> targetAction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITarget SetAsDefault()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITarget SetAsHidden()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITarget SetDescription(string description)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
