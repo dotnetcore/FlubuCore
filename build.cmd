c8145b634e4641fb33b20674ea69daf5922481bc
Zoro <zoro.zorec@inova.si>
2016-09-26 11:27:53 +0200

    updated local build script

--- build.cmd
+++ build.cmd
@@ -3,7 +3,11 @@ rmdir %userprofile%\.nuget\packages\flubu.core /S /Q
 rmdir %userprofile%\.nuget\packages\flubu /S /Q
 rmdir %userprofile%\.nuget\packages\.tools\dotnet-flubu /S /Q
 
-dotnet pack Flubu -c Release
-dotnet pack dotnet-flubu -c Release
+dotnet restore Flubu
+dotnet restore dotnet-flubu
+dotnet restore Flubu.Tests
 
-dotnet test Flubu.Tests
+dotnet pack Flubu -c Release --version-suffix %1
+dotnet pack dotnet-flubu -c Release --version-suffix %1
+
+dotnet test Flubu.Tests -xml results.xml
