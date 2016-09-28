4c50fe33527161e234b0e460587d530d0bb7c3cc
zoroz <zoro.zorec@gmail.com>
2016-09-28 21:11:56 +0200

    initial flubu script added

---
+++ BuildScript.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FlubuCore.Context;
+using FlubuCore.Scripting;
+using FlubuCore.Targeting;
+using FlubuCore.Tasks.NetCore;
+
+public class MyBuildScript : DefaultBuildScript
+{
+    protected override void ConfigureBuildProperties(ITaskSession session)
+    {
+    }
+
+    protected override void ConfigureTargets(ITaskSession session)
+    {
+        Target
+            .Create("compile")
+            .AddTask(new ExecuteDotnetTask("build").WithArguments("FlubuCore"))
+            .AddToTargetTree(session.TargetTree);
+    }
+}
