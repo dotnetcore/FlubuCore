d53996f6ddb3886ac4b9e83e7080bce3c046d8c9
zoroz <zoro.zorec@gmail.com>
2016-09-28 18:58:23 +0200

    updated build scripts

--- build.cmd
+++ build.cmd
@@ -1,13 +1,14 @@
 rmdir %userprofile%\.nuget\packages\dotnet-flubu /S /Q
 rmdir %userprofile%\.nuget\packages\flubu.core /S /Q
 rmdir %userprofile%\.nuget\packages\flubu /S /Q
+rmdir %userprofile%\.nuget\packages\FlubuCore /S /Q
 rmdir %userprofile%\.nuget\packages\.tools\dotnet-flubu /S /Q
 
-dotnet restore Flubu
+dotnet restore FlubuCore
 dotnet restore dotnet-flubu
 dotnet restore Flubu.Tests
 
-dotnet pack Flubu -c Release --version-suffix %1
+dotnet pack FlubuCore -c Release --version-suffix %1
 dotnet pack dotnet-flubu -c Release --version-suffix %1
 
 dotnet test Flubu.Tests -xml results.xml
