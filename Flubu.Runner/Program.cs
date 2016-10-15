41ad854d545dd6b63cfd2592fb636daa38f04581
Marko Zorec <mzorec@gmail.com>
2016-10-15 23:27:21 +0200

    flubu runner... not working

--- Flubu.Runner/Program.cs
+++ Flubu.Runner/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlubuCore.Commanding;
+using FlubuCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -14,9 +16,23 @@ namespace Flubu.Runner
 
         private static IServiceProvider _provider;
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            if (args == null)
+            {
+                args = new string[0];
+            }
 
+            Services
+                .AddCoreComponents()
+                .AddArguments(args);
+
+            _provider = Services.BuildServiceProvider();
+            ILoggerFactory factory = _provider.GetRequiredService<ILoggerFactory>();
+            factory.AddProvider(new FlubuLoggerProvider());
+
+            ICommandExecutor executor = _provider.GetRequiredService<ICommandExecutor>();
+            var x = executor.ExecuteAsync().Result;
         }
     }
 }
