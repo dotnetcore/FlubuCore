49cf68172d7fe229fc0ec8cc62ad5c51807b398f
Marko Zorec <mzorec@gmail.com>
2016-10-23 13:29:07 +0200

    Added CreateWebSiteTask

---
+++ FlubuCore/Tasks/Iis/CreateWebApplicationMode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlubuCore.Tasks.Iis
+{
+    public enum CreateWebApplicationMode
+    {
+        /// <summary>
+        /// If the application already exists, the task should fail.
+        /// </summary>
+        FailIfAlreadyExists,
+
+        /// <summary>
+        /// If the application already exists, it should be updated.
+        /// </summary>
+        UpdateIfExists,
+
+        /// <summary>
+        /// If the application already exists, the task should do nothing.
+        /// </summary>
+        DoNothingIfExists,
+    }
+}
