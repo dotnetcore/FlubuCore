7ce04852c1c828aaf53cfe9c6b00ffef00ec1361
Marko Zorec <mzorec@gmail.com>
2016-10-23 23:22:26 +0200

    added AddWebSiteBinding task

---
+++ FlubuCore/Tasks/Iis/AddWebSiteBindingTask.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlubuCore.Context;
+using FlubuCore.Tasks.Iis.Interfaces;
+using Microsoft.Web.Administration;
+
+namespace FlubuCore.Tasks.Iis
+{
+    public class AddWebsiteBindingTask : TaskBase, IAddWebsiteBindingTask
+    {
+        private string _siteName;
+
+        private string _bindProtocol;
+
+        private string _certificateStore;
+
+        private string _certificateHash;
+
+        public IAddWebsiteBindingTask SiteName(string name)
+        {
+            _siteName = name;
+            return this;
+        }
+
+        public IAddWebsiteBindingTask AddBinding(string protocol)
+        {
+            _bindProtocol = protocol;
+            return this;
+        }
+
+        public IAddWebsiteBindingTask CertificateStore(string store)
+        {
+            _certificateStore = store;
+            return this;
+        }
+
+        public IAddWebsiteBindingTask CertificateHash(string hash)
+        {
+            _certificateHash = hash;
+            return this;
+        }
+
+        protected override int DoExecute(ITaskContext context)
+        {
+            if (string.IsNullOrEmpty(_siteName))
+                throw new TaskExecutionException("Site name missing!", 1);
+            if (string.IsNullOrEmpty(_bindProtocol))
+                throw new TaskExecutionException("Protocol missing!", 1);
+            if (_bindProtocol.IndexOf("https", StringComparison.OrdinalIgnoreCase) >= 0 &&
+                (string.IsNullOrEmpty(_certificateStore) || string.IsNullOrEmpty(_certificateHash)))
+                throw new TaskExecutionException("Certificate store or hash not set for SSL protocol", 1);
+
+            using (ServerManager manager = new ServerManager())
+            {
+                Site site = manager.Sites[_siteName];
+
+                //// See if this binding is already on some site
+                if (manager.Sites.Where(st => st.Bindings.Where(b => b.Protocol == _bindProtocol).Any()).Any())
+                {
+                    context.LogInfo($"Binding for protocol '{_bindProtocol}' already exists! Doing nothing.");
+                    return 0;
+                }
+
+                Binding binding = site.Bindings.CreateElement();
+                binding.Protocol = _bindProtocol;
+                binding.CertificateStoreName = _certificateStore;
+                binding.CertificateHash = Encoding.UTF8.GetBytes(_certificateHash);
+                site.Bindings.Add(binding);
+
+                manager.CommitChanges();
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ FlubuCore/Tasks/Iis/Interfaces/IAddWebSiteBindingTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlubuCore.Tasks.Iis.Interfaces
+{
+    public interface IAddWebsiteBindingTask : ITask
+    {
+        IAddWebsiteBindingTask AddBinding(string protocol);
+
+        IAddWebsiteBindingTask SiteName(string name);
+
+        IAddWebsiteBindingTask CertificateStore(string store);
+
+        IAddWebsiteBindingTask CertificateHash(string hash);
+    }
+}
