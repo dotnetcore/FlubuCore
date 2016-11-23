536461a7f22ac26a13301986430e0a4011728dcb
zoroz <zoro.zorec@gmail.com>
2016-11-23 18:19:51 +0100

    added systemctl linux command task

---
+++ FlubuCore/Tasks/Linux/SystemCtlTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlubuCore.Context;
+using FlubuCore.Tasks.Process;
+
+namespace FlubuCore.Tasks.Linux
+{
+    public class SystemCtlTask : TaskBase<int>
+    {
+        private readonly string _command;
+        private readonly string _service;
+
+        public SystemCtlTask(string command, string service)
+        {
+            _command = command;
+            _service = service;
+        }
+
+        protected override int DoExecute(ITaskContext context)
+        {
+            IRunProgramTask task = context.Tasks().RunProgramTask("systemctl")
+                .WithArguments(_command, _service);
+
+            return task.Execute(context);
+        }
+    }
+}
