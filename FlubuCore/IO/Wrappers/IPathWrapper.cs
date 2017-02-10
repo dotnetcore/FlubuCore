ac2de2aa488257b430e5f5137cead405235034e5
Marko Zorec <markoz@comtrade.com>
2017-02-10 19:41:56 +0100

    Missing files

---
+++ FlubuCore/IO/Wrappers/IPathWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlubuCore.IO.Wrappers
+{
+    public interface IPathWrapper
+    {
+        string GetExtension(string path);
+    }
+}
---
+++ FlubuCore/IO/Wrappers/PathWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FlubuCore.IO.Wrappers
+{
+    public class PathWrapper : IPathWrapper
+    {
+        public string GetExtension(string path)
+        {
+            return Path.GetExtension(path);
+        }
+    }
+}
