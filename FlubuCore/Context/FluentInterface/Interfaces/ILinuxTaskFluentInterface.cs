2a4915dde0a2c56c76f38927493e5d2d69fad355
markoz <mzorec@gmail.com>
2017-02-25 12:10:58 +0100

    Linux and task extensions documentation.

--- FlubuCore/Context/FluentInterface/Interfaces/ILinuxTaskFluentInterface.cs
+++ FlubuCore/Context/FluentInterface/Interfaces/ILinuxTaskFluentInterface.cs
@@ -4,6 +4,12 @@ namespace FlubuCore.Context.FluentInterface.Interfaces
 {
     public interface ILinuxTaskFluentInterface
     {
+        /// <summary>
+        /// Run's system ctl.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="service"></param>
+        /// <returns></returns>
         SystemCtlTask SystemCtlTask(string command, string service);
     }
 }
--- FlubuCore/Context/FluentInterface/Interfaces/ITaskExtensionsFluentInterface.cs
+++ FlubuCore/Context/FluentInterface/Interfaces/ITaskExtensionsFluentInterface.cs
@@ -4,12 +4,32 @@ namespace FlubuCore.Context.FluentInterface.Interfaces
 {
     public interface ITaskExtensionsFluentInterface
     {
+        /// <summary>
+        /// Generate's common assembly info file. Information is taken from build properties cotnext.
+        /// </summary>
+        /// <returns></returns>
         ITaskExtensionsFluentInterface GenerateCommonAssemblyInfo();
 
+        /// <summary>
+        /// Run's multriple programs
+        /// </summary>
+        /// <param name="programs"></param>
+        /// <returns></returns>
         ITaskExtensionsFluentInterface RunMultiProgram(params string[] programs);
 
+        /// <summary>
+        /// Run specified program.
+        /// </summary>
+        /// <param name="program"></param>
+        /// <param name="workingFolder"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
         ITaskExtensionsFluentInterface RunProgram(string program, string workingFolder, params string[] args);
 
+        /// <summary>
+        ///  Moves back to target fluent interface.
+        /// </summary>
+        /// <returns></returns>
         ITargetFluentInterface BackToTarget();
     }
 }
