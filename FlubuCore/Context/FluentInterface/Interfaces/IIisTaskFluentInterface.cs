c48d18b800d84112b8b6a9116e52dbca3f4b68ad
markoz <mzorec@gmail.com>
2017-02-25 15:25:07 +0100

    IIs task fluent interface documentation

--- FlubuCore/Context/FluentInterface/Interfaces/IIisTaskFluentInterface.cs
+++ FlubuCore/Context/FluentInterface/Interfaces/IIisTaskFluentInterface.cs
@@ -5,16 +5,41 @@ namespace FlubuCore.Context.FluentInterface.Interfaces
 {
     public interface IIisTaskFluentInterface
     {
+        /// <summary>
+        /// Task creates new web site in iis.
+        /// </summary>
+        /// <returns></returns>
         ICreateWebsiteTask CreateWebsiteTask();
 
+        /// <summary>
+        /// Task created new web application on the specified web site in iis
+        /// </summary>
+        /// <returns></returns>
         ICreateWebApplicationTask CreateWebApplicationTask();
 
+        /// <summary>
+        /// Task creates new application pool in iis.
+        /// </summary>
+        /// <returns></returns>
         ICreateAppPoolTask CreateAppPoolTask();
 
+        /// <summary>
+        /// Task delentes specified Application pool.
+        /// </summary>
+        /// <returns></returns>
         IDeleteAppPoolTask DeleteAppPoolTask();
 
+        /// <summary>
+        /// /Task for controlling the application pool (start, stop)
+        /// </summary>
+        /// <returns></returns>
         IControlAppPoolTask ControlAppPoolTask();
 
+        /// <summary>
+        /// Task add binding to existing web site.
+        /// </summary>
+        /// <returns></returns>
         IAddWebsiteBindingTask AddWebsiteBindingTask();
+
     }
 }
