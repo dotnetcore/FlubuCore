2a181286c986e1cf3cd9bba26704516063616ddc
markoz <mzorec@gmail.com>
2017-07-11 10:10:57 +0200

    Removed flubu client from flubu core

--- dotnet-flubu/Infrastructure/InstallerExtensions.cs
+++ dotnet-flubu/Infrastructure/InstallerExtensions.cs
@@ -18,7 +18,6 @@ using FlubuCore.Tasks.NetCore;
 using FlubuCore.Tasks.Solution;
 using FlubuCore.Tasks.Testing;
 using FlubuCore.Tasks.Versioning;
-using FlubuCore.WebApi.Client;
 using Microsoft.DotNet.Cli.Utils;
 using Microsoft.Extensions.CommandLineUtils;
 using Microsoft.Extensions.DependencyInjection;
@@ -40,7 +39,7 @@ namespace DotNet.Cli.Flubu.Infrastructure
                 .AddSingleton<IFlubuEnviromentService, FlubuEnviromentService>()
                 .AddSingleton<ICommandFactory, CommandFactory>()
                 .AddSingleton<ITaskFactory, DotnetTaskFactory>()
-                .AddSingleton<IWebApiClient, WebApiClient>()
+                ////.AddSingleton<IWebApiClient, WebApiClient>()
                 .AddSingleton<HttpClient, HttpClient>();
 
             return services;
