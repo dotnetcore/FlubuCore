1df125506c7c8b5eb0d6225049d9e9b4c9ac7ede
markoz <mzorec@gmail.com>
2017-08-12 10:54:36 +0200

    Upload package task implemented.

---
+++ FlubuCore/Tasks/FlubuWebApi/UploadPackageTask.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FlubuCore.Context;
+using FlubuCore.WebApi.Client;
+using FlubuCore.WebApi.Model;
+
+namespace FlubuCore.Tasks.FlubuWebApi
+{
+    public class UploadPackageTask : TaskBase<int>
+    {
+	    private readonly IWebApiClient _webApiClient;
+
+	    private string _packageSearchPattern;
+
+	    private string _directoryPath;
+
+	    public UploadPackageTask(IWebApiClient client)
+	    {
+		    _webApiClient = client;
+	    }
+
+	    public UploadPackageTask DirectoryPath(string directoryPath)
+	    {
+		    this._directoryPath = directoryPath;
+		    return this;
+	    }
+
+	    public UploadPackageTask PackageSearchPattern(string packageSearchPattern)
+	    {
+		    _packageSearchPattern = packageSearchPattern;
+		    return this;
+	    }
+
+		protected override int DoExecute(ITaskContextInternal context)
+	    {
+		    Task<int> task = DoExecuteAsync(context);
+
+		    return task.GetAwaiter().GetResult();
+	    }
+
+	    protected override async Task<int> DoExecuteAsync(ITaskContextInternal context)
+	    {
+		    await _webApiClient.UploadPackageAsync(new UploadPackageRequest
+		    {
+			    PackageSearchPattern = _packageSearchPattern,
+				DirectoryPath = _directoryPath
+		    });
+
+		    return 0;
+	    }
+	}
+}
