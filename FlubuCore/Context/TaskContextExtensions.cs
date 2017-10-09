fc0e198784fe6593bbdb6f2f3535acea19816644
markoz <mzorec@gmail.com>
2017-10-09 09:18:12 +0200

    Added GetEnviromentVariable extension method.

---
+++ FlubuCore/Context/TaskContextExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlubuCore.Context
+{
+    public static class TaskContextExtensions
+    {
+        public static string GetEnvironmentVariable(this ITaskContext context, string variable)
+        {
+            return Environment.GetEnvironmentVariable(variable);
+        }
+    }
+}
