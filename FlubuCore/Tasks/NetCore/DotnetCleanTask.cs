80d2795bb6e65704eb13c0d6106e6f1efcdac078
markoz <mzorec@gmail.com>
2017-11-11 10:53:52 +0100

    Fixed dotnet clean configuration set from WithArguments and from build proprs.

--- FlubuCore/Tasks/NetCore/DotnetCleanTask.cs
+++ FlubuCore/Tasks/NetCore/DotnetCleanTask.cs
@@ -7,10 +7,6 @@ namespace FlubuCore.Tasks.NetCore
 {
     public class DotnetCleanTask : ExecuteDotnetTaskBase<DotnetCleanTask>
     {
-        protected bool _projectNameIsSet;
-
-        protected bool _configurationIsSet;
-
         public DotnetCleanTask() : base(StandardDotnetCommands.Clean)
         {
         }
@@ -22,8 +18,7 @@ namespace FlubuCore.Tasks.NetCore
         /// <returns></returns>
         public DotnetCleanTask Project(string projectName)
         {
-            WithArguments(projectName);
-            _projectNameIsSet = true;
+            Arguments.Insert(0, projectName);
             return this;
         }
 
@@ -46,13 +41,12 @@ namespace FlubuCore.Tasks.NetCore
         public DotnetCleanTask Configuration(string configuration)
         {
             WithArguments("-c", configuration);
-            _configurationIsSet = true;
             return this;
         }
 
         protected override void BeforeExecute(ITaskContextInternal context)
         {
-            if (!_projectNameIsSet)
+            if (Arguments.Count == 0 || Arguments[0].StartsWith("-"))
             {
                 var solustionFileName = context.Properties.Get<string>(BuildProps.SolutionFileName, null);
                 if (solustionFileName != null)
@@ -61,7 +55,7 @@ namespace FlubuCore.Tasks.NetCore
                 }
             }
 
-            if (!_configurationIsSet)
+            if (!Arguments.Exists(x => x == "-c" || x == "--configuration"))
             {
                 var configuration = context.Properties.Get<string>(BuildProps.BuildConfiguration, null);
                 if (configuration != null)
