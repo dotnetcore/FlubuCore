40c0a46213234248ff9360e7e47c974b7d04b4d5
markoz <mzorec@gmail.com>
2017-11-27 20:37:56 +0100

    Revert "Added FromArgument unit tests."
    This reverts commit cb835ca553d84a155d8ea7b4f2934cdacad97dcc.

--- Flubu.Tests/Tasks/BaseTaskUnitTests.cs
+++ Flubu.Tests/Tasks/BaseTaskUnitTests.cs
@@ -1,78 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using FlubuCore;
 using FlubuCore.Services;
 using Moq;
-using Xunit;
 
 namespace Flubu.Tests.Tasks
 {
-    public class BaseTaskUnitTests : TaskUnitTestBase
+    public class BaseTaskUnitTests
     {
-        private SimpleTask _task;
+        private SimpleTask _simpleTask;
 
         private Mock<IFlubuEnviromentService> flubuEnviromentService;
 
         public BaseTaskUnitTests()
         {
-            flubuEnviromentService = new Mock<IFlubuEnviromentService>();
-            _task = new SimpleTask(flubuEnviromentService.Object);
-        }
-
-        [Fact]
-        public void FromArgument_StringValueFromArgumentToStringParameter_Succesfull()
-        {
-            Context.Setup(x => x.ScriptArgs).Returns(new DictionaryWithDefault<string, string>()
-            {
-                {"-s", "value from arg"}
-            });
-
-            _task.FromArgument(x => x.AddPath("default vaue"), "-s");
-            _task.Execute(Context.Object);
-            Assert.Equal("value from arg", _task.Path);
-        }
-
-        [Fact]
-        public void FromArgument_IntValueFromArgumentToIntParameter_Succesfull()
-        {
-            Context.Setup(x => x.ScriptArgs).Returns(new DictionaryWithDefault<string, string>()
-            {
-                { "-l", "2" }
-            });
-
-            _task.FromArgument(x => x.SetLevel(0), "-l", "help bla bla");
-          
-            _task.Execute(Context.Object);
-            Assert.Equal(2, _task.Level);
-        }
-
-        [Fact]
-        public void FromArgument_DefaultValue_Succesfull()
-        {
-            Context.Setup(x => x.ScriptArgs).Returns(new DictionaryWithDefault<string, string>()
-            {
-                { "-l", "2" },
-            });
-
-            Context.Setup(x => x.ScriptArgs).Returns(new DictionaryWithDefault<string, string>());
-
-            _task.FromArgument(x => x.AddPath("default value"), "-s");
-            _task.Execute(Context.Object);
-            Assert.Equal("default value", _task.Path);
-        }
-
-
-        [Fact(Skip = "Not implemented yet")]
-        public void FromArgument_StringValueFromArgumentToIntParameter_Throws()
-        {
-            Context.Setup(x => x.ScriptArgs).Returns(new DictionaryWithDefault<string, string>()
-            {
-                { "-l", "abc" }
-            });
-
-            _task.FromArgument(x => x.SetLevel(-1), "-l", "help bla bla");
-            _task.Execute(Context.Object);
+            _simpleTask = new SimpleTask(flubuEnviromentService.Object);
         }
     }
 }
