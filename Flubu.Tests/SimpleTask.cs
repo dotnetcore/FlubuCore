4dba124a50583bdb91f67f2e20f20f2bbd4839ce
markoz <mzorec@gmail.com>
2017-11-27 20:41:49 +0100

    Revert "build fix."
    This reverts commit 50452e6c6343f6ab9c24a80fc47a9cf5427dcc10.

--- Flubu.Tests/SimpleTask.cs
+++ Flubu.Tests/SimpleTask.cs
@@ -2,22 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 using FlubuCore.Context;
-using FlubuCore.Services;
 using FlubuCore.Tasks;
 
 namespace Flubu.Tests
 {
     public class SimpleTask : TaskBase<int, SimpleTask>
     {
-        private IFlubuEnviromentService _flubuEnviromentService;
-        public SimpleTask(IFlubuEnviromentService flubuEnviromentService)
-        {
-            _flubuEnviromentService = flubuEnviromentService;
-        }
-
         protected override int DoExecute(ITaskContextInternal context)
         {
-            _flubuEnviromentService.ListAvailableMSBuildToolsVersions();
             return 0;
         }
     }
