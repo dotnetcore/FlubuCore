517216552904de925f34c48d388b00c7d878677a
markoz <mzorec@gmail.com>
2017-11-27 22:12:57 +0100

    Revert "Revert "Added FromArgument unit tests.""
    This reverts commit 40c0a46213234248ff9360e7e47c974b7d04b4d5.

--- Flubu.Tests/Tasks/BaseTaskUnitTests.cs
+++ Flubu.Tests/Tasks/BaseTaskUnitTests.cs
@@ -1,20 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FlubuCore;
 using FlubuCore.Services;
 using Moq;
+using Xunit;
 
 namespace Flubu.Tests.Tasks
 {
-    public class BaseTaskUnitTests
+    public class BaseTaskUnitTests : TaskUnitTestBase
     {
-        private SimpleTask _simpleTask;
+        private SimpleTask _task;
 
         private Mock<IFlubuEnviromentService> flubuEnviromentService;
 
         public BaseTaskUnitTests()
         {
-            _simpleTask = new SimpleTask(flubuEnviromentService.Object);
+            flubuEnviromentService = new Mock<IFlubuEnviromentService>();
+            _task = new SimpleTask(flubuEnviromentService.Object);
+        }
+
+        [Fact]
+        public void FromArgument_StringValueFromArgumentToStringParameter_Succesfull()
+        {
+            Context.Setup(x => x.ScriptArgs).Returns(new DictionaryWithDefault<string, string>()
+            {
+                {"-s", "value from arg"}
+            });
+
+            _task.FromArgument(x => x.AddPath("default vaue"), "-s");
+            _task.Execute(Context.Object);
+            Assert.Equal("value from arg", _task.Path);
+        }
+
+        [Fact]
+        public void FromArgument_IntValueFromArgumentToIntParameter_Succesfull()
+        {
+            Context.Setup(x => x.ScriptArgs).Returns(new DictionaryWithDefault<string, string>()
+            {
+                { "-l", "2" }
+            });
+
+            _task.FromArgument(x => x.SetLevel(0), "-l", "help bla bla");
+          
+            _task.Execute(Context.Object);
+            Assert.Equal(2, _task.Level);
+        }
+
+        [Fact]
+        public void FromArgument_DefaultValue_Succesfull()
+        {
+            Context.Setup(x => x.ScriptArgs).Returns(new DictionaryWithDefault<string, string>()
+            {
+                { "-l", "2" },
+            });
+
+            Context.Setup(x => x.ScriptArgs).Returns(new DictionaryWithDefault<string, string>());
+
+            _task.FromArgument(x => x.AddPath("default value"), "-s");
+            _task.Execute(Context.Object);
+            Assert.Equal("default value", _task.Path);
+        }
+
+
+        [Fact(Skip = "Not implemented yet")]
+        public void FromArgument_StringValueFromArgumentToIntParameter_Throws()
+        {
+            Context.Setup(x => x.ScriptArgs).Returns(new DictionaryWithDefault<string, string>()
+            {
+                { "-l", "abc" }
+            });
+
+            _task.FromArgument(x => x.SetLevel(-1), "-l", "help bla bla");
+            _task.Execute(Context.Object);
         }
     }
 }
