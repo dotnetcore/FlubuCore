ea840aeed5fa7f7277d94bd500fc480a36b1d53e
zoroz <zoro.zorec@comtrade.com>
2017-12-06 10:43:16 +0100

    fixed dotnet executable resolution if not installed

--- FlubuCore/Tasks/NetCore/Dotnet.cs
+++ FlubuCore/Tasks/NetCore/Dotnet.cs
@@ -1,7 +1,6 @@
 ﻿using System.IO;
 using System.Runtime.InteropServices;
 using FlubuCore.IO;
-using FlubuCore.Targeting;
 
 namespace FlubuCore.Tasks.NetCore
 {
@@ -33,18 +32,12 @@ namespace FlubuCore.Tasks.NetCore
         public static string FindDotnetExecutable()
         {
             bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-            string dotnetExecutable;
 
-            if (isWindows)
-            {
-                dotnetExecutable = File.Exists("C:/Program Files/dotnet/dotnet.exe") ? "C:/Program Files/dotnet/dotnet.exe" : null;
-            }
-            else
-            {
-                dotnetExecutable = "/usr/bin/dotnet";
-            }
+            string dotnetExecutable = isWindows
+                ? (File.Exists("C:/Program Files/dotnet/dotnet.exe") ? "C:/Program Files/dotnet/dotnet.exe" : null)
+                : "/usr/bin/dotnet";
 
-            return IOExtensions.GetFullPath(dotnetExecutable);
+            return string.IsNullOrEmpty(dotnetExecutable) ? null : IOExtensions.GetFullPath(dotnetExecutable);
         }
     }
 }
