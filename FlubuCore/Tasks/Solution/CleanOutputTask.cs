8b14c82200066cef4217f6855ebd7b8afe1c8720
markoz <mzorec@gmail.com>
2018-01-29 21:39:28 +0100

    Improved cleanoutput task

--- FlubuCore/Tasks/Solution/CleanOutputTask.cs
+++ FlubuCore/Tasks/Solution/CleanOutputTask.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using FlubuCore.Context;
 using FlubuCore.IO;
@@ -11,6 +12,8 @@ namespace FlubuCore.Tasks.Solution
     {
         private bool _cleanBuildDir;
         private string _description;
+        private bool _cleanOutputDir;
+        private List<(string path, bool recreate)> _directoriesToClean = new List<(string path, bool recreate)>();
 
         protected override string Description
         {
@@ -27,6 +30,32 @@ namespace FlubuCore.Tasks.Solution
             set => _description = value;
         }
 
+        /// <summary>
+        /// Task deletes added directory
+        /// </summary>
+        /// <param name="directory">The directory do delete</param>
+        /// <param name="recreate">If <c>true</c> directory is recreated. Otherwise deleted.</param>
+        /// <returns></returns>
+        public CleanOutputTask AddDirectoryToClean(string directory, bool recreate)
+        {
+            _directoriesToClean.Add((directory, recreate));
+            return this;
+        }
+
+        /// <summary>
+        /// If set OutputDir specified in <see cref="BuildProps.OutputDir"/> deleted and recreated.
+        /// </summary>
+        /// <returns></returns>
+        public CleanOutputTask CleanOutputDir()
+        {
+            _cleanOutputDir = true;
+            return this;
+        }
+
+        /// <summary>
+        /// If set BuildDir specified in <see cref="BuildProps.BuildDir"/> deleted and recreated.
+        /// </summary>
+        /// <returns></returns>
         public CleanOutputTask CleanBuildDir()
         {
             _cleanBuildDir = true;
@@ -67,6 +96,27 @@ namespace FlubuCore.Tasks.Solution
             {
                 CreateDirectoryTask createDirectoryTask = new CreateDirectoryTask(buildDir, true);
                 createDirectoryTask.Execute(context);
+            }
+
+            string outputDir = context.Properties.Get<string>(BuildProps.OutputDir);
+            if (!string.IsNullOrEmpty(outputDir) && _cleanOutputDir)
+            {
+                CreateDirectoryTask createDirectoryTask = new CreateDirectoryTask(outputDir, true);
+                createDirectoryTask.Execute(context);
+            }
+
+            foreach (var dir in _directoriesToClean)
+            {
+                if (dir.recreate)
+                {
+                    CreateDirectoryTask createDirectoryTask = new CreateDirectoryTask(dir.path, true);
+                    createDirectoryTask.Execute(context);
+                }
+                else
+                {
+                    DeleteDirectoryTask task = new DeleteDirectoryTask(dir.path, false);
+                    task.Execute(context);
+                }
             }
 
             return 0;
