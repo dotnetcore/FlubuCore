f987d38b405a47ad4abf595d7166e252861ce49c
markoz <mzorec@gmail.com>
2018-02-23 01:27:32 +0100

    Added ping controller

---
+++ FlubuCore.WebApi/Controllers/PingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlubuCore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class PingController : Controller
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok();
+        }
+    }
+}
