038b84c40d8264fa8dc6f37be862c58b23a56e95
markoz <mzorec@gmail.com>
2018-02-27 14:58:27 +0100

    Added command arguments documentation

--- FlubuCore/Scripting/CommandArguments.cs
+++ FlubuCore/Scripting/CommandArguments.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using FlubuCore.Context;
 using FlubuCore.Infrastructure;
 
 namespace FlubuCore.Scripting
@@ -7,6 +8,9 @@ namespace FlubuCore.Scripting
     {
         public bool Help { get; set; }
 
+        /// <summary>
+        /// If <c>true</c> dotnet-flubu cli tool and flubu.runner shows whole excetpion stacktrace. otherwise only message.
+        /// </summary>
         public bool Debug { get; set; }
 
         /// <summary>
@@ -14,16 +18,31 @@ namespace FlubuCore.Scripting
         /// </summary>
         public bool NoDependencies { get; set; }
 
+        /// <summary>
+        /// The list of main commands (targets) to be executed.
+        /// </summary>
         public List<string> MainCommands { get; set; }
 
         public List<string> RemainingCommands { get; set; } = new List<string>();
 
+        /// <summary>
+        /// Location where <see cref="BuildScriptLocator"/> searches for build script.
+        /// </summary>
         public string Script { get; set; }
 
+        /// <summary>
+        /// List of all targets(also dependencies) that must be executed. If they are not script execution fails.
+        /// </summary>
         public List<string> TargetsToExecute { get; set; }
 
+        /// <summary>
+        /// If <c>true</c> Target's provided in <see cref="MainCommands"/> are executed in parallel.
+        /// </summary>
         public bool ExecuteTargetsInParallel { get; set; }
 
+        /// <summary>
+        /// Script Argument that can be accessed in build script through <see cref="IBuildPropertiesContext.ScriptArgs"/>
+        /// </summary>
         public DictionaryWithDefault<string, string> ScriptArguments { get; set; } = new DictionaryWithDefault<string, string>();
 
         /// <summary>
@@ -37,6 +56,9 @@ namespace FlubuCore.Scripting
         /// </summary>
         public bool RethrowOnException { get; set; }
 
+        /// <summary>
+        /// Location's where <see cref="ScriptLoader"/> looks for assemblies to load.
+        /// </summary>
         public List<string> AssemblyDirectories { get; set; } = new List<string>();
     }
 }
