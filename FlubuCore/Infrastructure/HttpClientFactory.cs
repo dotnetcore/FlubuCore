bc076541447c4f74dae29e6eb36a83278c086425
markoz <mzorec@gmail.com>
2018-02-27 13:20:12 +0100

    Implemented http client factory.

---
+++ FlubuCore/Infrastructure/HttpClientFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace FlubuCore.Infrastructure
+{
+    public class HttpClientFactory : IHttpClientFactory
+    {
+        private static IDictionary<string, HttpClient> _clients = new Dictionary<string, HttpClient>();
+
+        public HttpClient Create(string endpoint)
+        {
+            HttpClient client;
+
+            if (_clients.TryGetValue(endpoint, out client))
+            {
+                return client;
+            }
+
+            client = new HttpClient
+            {
+                BaseAddress = new Uri(endpoint),
+            };
+            _clients[endpoint] = client;
+
+            return client;
+        }
+    }
+}
---
+++ FlubuCore/Infrastructure/IHttpClientFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace FlubuCore.Infrastructure
+{
+    public interface IHttpClientFactory
+    {
+        HttpClient Create(string endpoint);
+    }
+}
