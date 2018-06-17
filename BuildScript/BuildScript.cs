c07e3555c3f5912df38462f721ada576f54a2e56
markoz <mzorec@gmail.com>
2018-06-17 23:14:20 +0200

    added back web api tests to linux target.

--- BuildScript/BuildScript.cs
+++ BuildScript/BuildScript.cs
@@ -103,7 +103,7 @@ public class BuildScript : DefaultBuildScript
         var flubuTestsLinux = context.CreateTarget("test.linux")
             .SetDescription("Runs all tests in solution.")
             .AddCoreTask(x => x.Test().Project("Flubu.Tests\\Flubu.Tests.csproj").WithArguments("--filter", "Category!=OnlyWindows"));
-            ////.AddCoreTask(x => x.Test().Project("FlubuCore.WebApi.Tests\\FlubuCore.WebApi.Tests.csproj"));
+            .AddCoreTask(x => x.Test().Project("FlubuCore.WebApi.Tests\\FlubuCore.WebApi.Tests.csproj"));
 
         context.CreateTarget("rebuild.linux")
             .SetDescription("Rebuilds the solution and publishes nuget packages.")
