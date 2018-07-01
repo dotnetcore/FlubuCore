fdb75c9fb3a0b8b348bd1551080e63d8c57c67b9
markoz <mzorec@gmail.com>
2018-07-01 10:49:56 +0200

    refactored global tools.

--- FlubuCore.GlobalTool/Program.cs
+++ FlubuCore.GlobalTool/Program.cs
@@ -9,30 +9,9 @@ namespace FlubuCore.GlobalTool
 {
     public class Program
     {
-        private static readonly IServiceCollection Services = new ServiceCollection();
-
-        private static IServiceProvider _provider;
-
         public static int Main(string[] args)
         {
-            if (args == null)
-            {
-                args = new string[0];
-            }
-
-            Services
-                .AddCoreComponents()
-                .AddCommandComponents()
-                .AddScriptAnalyser()
-                .AddArguments(args)
-                .AddTasks();
-
-            _provider = Services.BuildServiceProvider();
-            ILoggerFactory factory = _provider.GetRequiredService<ILoggerFactory>();
-            factory.AddProvider(new FlubuLoggerProvider());
-
-            ICommandExecutor executor = _provider.GetRequiredService<ICommandExecutor>();
-            return executor.ExecuteAsync().Result;
+            return DotNet.Cli.Flubu.Program.Main(args);
         }
     }
 }
