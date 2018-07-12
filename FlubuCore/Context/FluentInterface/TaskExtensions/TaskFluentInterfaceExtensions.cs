4d3d43fa1a282378d28a007e5ff31be56f5c65ff
markoz <mzorec@gmail.com>
2018-07-13 01:06:14 +0200

    Added DoTaskAsync as task extension to  task fluent interface.

--- FlubuCore/Context/FluentInterface/TaskExtensions/TaskFluentInterfaceExtensions.cs
+++ FlubuCore/Context/FluentInterface/TaskExtensions/TaskFluentInterfaceExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using FlubuCore.Context.FluentInterface.Interfaces;
 using FlubuCore.Tasks;
 using FlubuCore.Tasks.Process;
@@ -38,6 +39,36 @@ namespace FlubuCore.Context.FluentInterface.TaskExtensions
         public static DoTask6<T, T2, T3, T4, T5> Do<T, T2, T3, T4, T5>(this ITaskFluentInterface taskF, Action<ITaskContextInternal, T, T2, T3, T4, T5> taskAction, T param, T2 param2, T3 param3, T4 param4, T5 param5)
         {
             return new DoTask6<T, T2, T3, T4, T5>(taskAction, param, param2, param3, param4, param5);
+        }
+
+        public static DoTaskAsync DoAsync(this ITaskFluentInterface taskF, Func<ITaskContext, Task> taskAction)
+        {
+            return new DoTaskAsync(taskAction);
+        }
+
+        public static DoTaskAsync2<T> DoAsync<T>(this ITaskFluentInterface taskF, Func<ITaskContextInternal, T, Task> taskAction, T param)
+        {
+            return new DoTaskAsync2<T>(taskAction, param);
+        }
+
+        public static DoTaskAsync3<T, T2> DoAsync<T, T2>(this ITaskFluentInterface taskF, Func<ITaskContextInternal, T, T2, Task> taskAction, T param, T2 param2)
+        {
+            return new DoTaskAsync3<T, T2>(taskAction, param, param2);
+        }
+
+        public static DoTaskAsync4<T, T2, T3> DoAsync<T, T2, T3>(this ITaskFluentInterface taskF, Func<ITaskContextInternal, T, T2, T3, Task> taskAction, T param, T2 param2, T3 param3)
+        {
+            return new DoTaskAsync4<T, T2, T3>(taskAction, param, param2, param3);
+        }
+
+        public static DoTaskAsync5<T, T2, T3, T4> DoAsync<T, T2, T3, T4>(this ITaskFluentInterface taskF, Func<ITaskContextInternal, T, T2, T3, T4, Task> taskAction, T param, T2 param2, T3 param3, T4 param4)
+        {
+            return new DoTaskAsync5<T, T2, T3, T4>(taskAction, param, param2, param3, param4);
+        }
+
+        public static DoTaskAsync6<T, T2, T3, T4, T5> DoAsync<T, T2, T3, T4, T5>(this ITaskFluentInterface taskF, Func<ITaskContextInternal, T, T2, T3, T4, T5, Task> taskAction, T param, T2 param2, T3 param3, T4 param4, T5 param5)
+        {
+            return new DoTaskAsync6<T, T2, T3, T4, T5>(taskAction, param, param2, param3, param4, param5);
         }
 
         /// <summary>
