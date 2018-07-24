3c501ef73dc4e8e7e25935fc1e2caca1b6b986dd
markoz <mzorec@gmail.com>
2018-07-24 16:29:45 +0200

    build fix.

--- FlubuCore/Scripting/ScriptLoader.cs
+++ FlubuCore/Scripting/ScriptLoader.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Runtime.Loader;
 #if NETSTANDARD1_6
 using System.Runtime.Loader;
 #endif
