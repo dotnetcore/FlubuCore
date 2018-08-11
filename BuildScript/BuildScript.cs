a9ae8eda3dad2047a34595acfa2f8874dc133905
markoz <mzorec@gmail.com>
2018-08-11 19:36:29 +0200

    published FlubuCore.Analyzer 1.0.3

--- BuildScript/BuildScript.cs
+++ BuildScript/BuildScript.cs
@@ -186,7 +186,7 @@ public class BuildScript : DefaultBuildScript
         context.CoreTasks().ExecuteDotnetTask("nuget")
             .DoNotFailOnError(e => { Console.WriteLine($"Failed to publish FlubuCore.Analyzer. exception: {e.Message}"); })
             .WithArguments("push")
-            .WithArguments($"output\\FlubuCore.Analyzers.1.0.2.nupkg")
+            .WithArguments($"output\\FlubuCore.Analyzers.1.0.3.nupkg")
             .WithArguments("-s", "https://www.nuget.org/api/v2/package")
             .WithArguments("-k", key).Execute(context);
 
