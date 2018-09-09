8b6031b4473e3b94f9c48a42179351f8f32794d9
Marko Zorec <mzorec@gmail.com>
2018-09-09 07:42:31 +0200

    Adds task validation extensions.

---
+++ FlubuCore/Tasks/TaskValidationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlubuCore.Context;
+
+namespace FlubuCore.Tasks
+{
+    internal static class TaskExtensions
+    {
+        public static void MustNotBeNullOrEmpty(this string parameter, string validationMessage)
+        {
+            if (string.IsNullOrEmpty(parameter))
+            {
+                throw new TaskExecutionException(validationMessage, 0);
+            }
+        }
+    }
+}
