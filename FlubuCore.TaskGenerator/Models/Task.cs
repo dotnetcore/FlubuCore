f10bfc298be0ebb7439f6e321767fbe9c244323a
markoz <mzorec@gmail.com>
2018-10-01 10:39:43 +0200

    Fixes some task properties.

--- FlubuCore.TaskGenerator/Models/Task.cs
+++ FlubuCore.TaskGenerator/Models/Task.cs
@@ -8,9 +8,12 @@ namespace FlubuCore.TaskGenerator.Models
 
         public string TaskName { get; set; }
 
+        // ReSharper disable once InconsistentNaming
         public string TResult { get; set; } = "int";
 
-        public List<Parameter> ConstructorParameters { get; }
+        public string ConstuctorSummary { get; set; }
+
+        public List<Parameter> ConstructorParameters { get; set; }
 
         public List<Method> Methods { get; set; }
      }
