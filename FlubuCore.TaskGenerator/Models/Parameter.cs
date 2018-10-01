388441cb04b7c14846857c7391cfbe9bf9d3d1ef
markoz <mzorec@gmail.com>
2018-10-01 13:01:46 +0200

    wip!

--- FlubuCore.TaskGenerator/Models/Parameter.cs
+++ FlubuCore.TaskGenerator/Models/Parameter.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FlubuCore.TaskGenerator
+﻿namespace FlubuCore.TaskGenerator.Models
 {
     public class Parameter
     {
--- FlubuCore.TaskGenerator/Models/Task.cs
+++ FlubuCore.TaskGenerator/Models/Task.cs
@@ -8,12 +8,14 @@ namespace FlubuCore.TaskGenerator.Models
 
         public string TaskName { get; set; }
 
+        public string ProjectName { get; set; }
+
         // ReSharper disable once InconsistentNaming
         public string TResult { get; set; } = "int";
 
         public string ConstuctorSummary { get; set; }
 
-        public List<Parameter> ConstructorParameters { get; set; }
+        
 
         public List<Method> Methods { get; set; }
      }
