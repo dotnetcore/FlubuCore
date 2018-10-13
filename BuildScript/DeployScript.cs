b69ffd4908e1c86681c264ab578ea9d37304bbc7
markoz <mzorec@gmail.com>
2018-10-13 14:44:53 +0200

    deploy script generates secure password

--- BuildScript/DeployScript.cs
+++ BuildScript/DeployScript.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 using FlubuCore.Context;
 using FlubuCore.Scripting;
 using FlubuCore.WebApi;
@@ -50,7 +52,7 @@ namespace DeploymentScript
             }
             else
             {
-                var liteDbPassword = GenerateRandomString(12);
+                var liteDbPassword = GenerateRandomSecureString(15);
                 connectionString = $"FileName=database.db; Password={liteDbPassword}";
             }
 
@@ -194,6 +196,41 @@ else
             }
 
             return new string(stringChars);
+        }
+
+        private string GenerateRandomSecureString(int length, string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") {
+            if (length < 0)
+                throw new ArgumentOutOfRangeException("length", "length cannot be less than zero.");
+
+            if (string.IsNullOrEmpty(allowedChars))
+                throw new ArgumentException("allowedChars may not be empty.");
+
+            const int byteSize = 0x100;
+            var allowedCharSet = new HashSet<char>(allowedChars).ToArray();
+            if (byteSize < allowedCharSet.Length) 
+                throw new ArgumentException(String.Format("allowedChars may contain no more than {0} characters.", byteSize));
+
+            // Guid.NewGuid and System.Random are not particularly random. By using a
+            // cryptographically-secure random number generator, the caller is always
+            // protected, regardless of use.
+     
+            using (var rng = System.Security.Cryptography.RandomNumberGenerator.Create()) {
+                var result = new StringBuilder();
+                var buf = new byte[128];
+                while (result.Length < length) {
+                    rng.GetBytes(buf);
+                    for (var i = 0; i < buf.Length && result.Length < length; ++i) {
+                        // Divide the byte into allowedCharSet-sized groups. If the
+                        // random value falls into the last group and the last group is
+                        // too small to choose from the entire allowedCharSet, ignore
+                        // the value in order to avoid biasing the result.
+                        var outOfRangeStart = byteSize - (byteSize % allowedCharSet.Length);
+                        if (outOfRangeStart <= buf[i]) continue;
+                        result.Append(allowedCharSet[buf[i] % allowedCharSet.Length]);
+                    }
+                }
+                return result.ToString();
+            }
         }
 
         public class DeploymentConfig
