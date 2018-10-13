9b65de4dc59b68e5130b1ebda70dbec8136301f5
markoz <mzorec@gmail.com>
2018-10-13 14:17:45 +0200

    Adds Parent

--- FlubuCore.TaskGenerator/Models/Task.cs
+++ FlubuCore.TaskGenerator/Models/Task.cs
@@ -8,6 +8,8 @@ namespace FlubuCore.TaskGenerator.Models
 
         public string TaskName { get; set; }
 
+        public string Parent { get; set; }
+
         public string ProjectName { get; set; }
 
         public string Namespace { get; set; }
