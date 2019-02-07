4280d113d413768121dd8314df1f95dbeb163e5d
markoz <mzorec@gmail.com>
2019-02-07 02:45:45 +0100

    Initial work on update center

---
+++ FlubuCore.WebApi/Controllers/UpdateCenterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlubuCore.WebApi.Controllers
+{
+    [Route("[controller]")]
+    public class UpdateCenterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
