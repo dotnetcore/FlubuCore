f6a39b8664f3835733086062b5f9fdd6e301e42b
markoz <mzorec@gmail.com>
2019-02-08 10:58:49 +0100

    Login page.

---
+++ FlubuCore.WebApi/Controllers/WebApp/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlubuCore.WebApi.Controllers.WebApp
+{
+    [Route("[controller]")]
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+#if NETCOREAPP1_1
+             return View("NotSupported");
+#else
+            //var claims = new List<Claim>()
+            //{
+            //};
+
+            //var userIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            //var userPrincipal = new ClaimsPrincipal(userIdentity);
+            //await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(userPrincipal));
+            return View();
+#endif
+        }
+    }
+}
---
+++ FlubuCore.WebApi/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlubuCore.WebApi.Models.ViewModels
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
