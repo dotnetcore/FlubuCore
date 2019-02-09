db89fe99e9f02fb8a48bdb428abe08b7d211fd0c
markoz <mzorec@gmail.com>
2019-02-09 08:51:08 +0100

    UpdateCenter: downloads latest release from github releases.

--- FlubuCore.WebApi/Controllers/WebApp/UpdateCenterController.cs
+++ FlubuCore.WebApi/Controllers/WebApp/UpdateCenterController.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Text;
 using System.Threading.Tasks;
 using FlubuCore.WebApi.Models.ViewModels;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -19,6 +26,10 @@ namespace FlubuCore.WebApi.Controllers.WebApp
 
     public class UpdateCenterController : Controller
     {
+        private readonly string _owner = "flubu-core";
+
+        private readonly string _reponame = "flubu.core";
+
         private readonly IGitHubClient _client;
 
         public UpdateCenterController(IGitHubClient client)
@@ -26,14 +37,13 @@ namespace FlubuCore.WebApi.Controllers.WebApp
             _client = client;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var owner = "flubu-core";
-            var reponame = "flubu.core";
-            var releases = await _client.Repository.Release.GetLatest(owner, reponame);
+            var latestRelease = await _client.Repository.Release.GetLatest(_owner, _reponame);
             var currentVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyFileVersionAttribute>().Version;
-            var latestVersion = releases.TagName;
-
+            var latestVersion = latestRelease.TagName;
+            string frameworkName = Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName;
             var current = new Version(currentVersion);
             var latest = new Version(latestVersion);
 
@@ -45,6 +55,61 @@ namespace FlubuCore.WebApi.Controllers.WebApp
             };
 
             return View(model);
+        }
+
+        [HttpGet("Update")]
+        public async Task Update()
+        {
+            string frameworkName = Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName;
+            var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            bool isNetCore = frameworkName.StartsWith(".NETCoreApp");
+
+            var latestRelease = await _client.Repository.Release.GetLatest(_owner, _reponame);
+            List<ReleaseAsset> assets;
+            if (isWindows)
+            {
+                assets = latestRelease.Assets.Where(x => x.Name.Contains("Windows")).ToList();
+            }
+            else
+            {
+                assets = latestRelease.Assets.Where(x => x.Name.Contains("Linux")).ToList();
+            }
+
+            ReleaseAsset asset = null;
+
+            if (isNetCore)
+            {
+                string netCoreVersion = frameworkName.Replace(".NETCoreApp,Version=", string.Empty);
+                switch (netCoreVersion.ToLowerInvariant())
+                {
+                    case "v2.1":
+                    {
+                        asset = assets.FirstOrDefault(x => x.Name.Contains("NetCoreApp2.1"));
+                        break;
+                    }
+
+                    case "v2.0":
+                    {
+                        asset = assets.FirstOrDefault(x => x.Name.Contains("NetCoreApp2.0"));
+                        break;
+                    }
+
+                    case "v1.1":
+                    {
+                        asset = assets.FirstOrDefault(x => x.Name.Contains("NetCoreApp1.1"));
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                asset = assets.FirstOrDefault(x => x.Name.Contains("Net462"));
+            }
+#if !NETCOREAPP1_1
+            var wc = new WebClient();
+            var filename = "Release.zip";
+            await wc.DownloadFileTaskAsync(asset.BrowserDownloadUrl, filename);
+#endif
         }
     }
 }
