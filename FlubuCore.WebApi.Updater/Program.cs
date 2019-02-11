03698127883b12869818208ffba9c4d60cacaa39
markoz <mzorec@gmail.com>
2019-02-11 20:40:23 +0100

    Flubu update for .net

--- FlubuCore.WebApi.Updater/Program.cs
+++ FlubuCore.WebApi.Updater/Program.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 
 namespace FlubuCore.WebApi.Updater
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Thread.Sleep(1000);
+            var flubuPath = Path.GetFullPath("Updates/WebApi/flubu.exe");
+            var deployScript = Path.GetFullPath("Updates/WebApi/DeployScript.cs");
+            Console.WriteLine($"path: {deployScript}");
+            var process = Process.Start(new ProcessStartInfo
+            {
+                WorkingDirectory = Path.GetDirectoryName(flubuPath),
+                FileName = flubuPath,
+                Arguments = $"-s={deployScript}"
+            });
+         
+            process.WaitForExit();
+            int code = process.ExitCode;
+            Console.WriteLine($"flubu exit code: {code}");
         }
     }
 }
