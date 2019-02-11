35488cca2a60e1548b684d6f53fe721472c2280c
markoz <mzorec@gmail.com>
2019-02-11 21:50:38 +0100

    FlubuCore WebApi update for .net core.

--- FlubuCore.WebApi.Updater/Program.cs
+++ FlubuCore.WebApi.Updater/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 using System.Threading;
 
 namespace FlubuCore.WebApi.Updater
@@ -9,14 +12,47 @@ namespace FlubuCore.WebApi.Updater
     {
         public static void Main(string[] args)
         {
-            Thread.Sleep(1000);
-            var flubuPath = Path.GetFullPath("Updates/WebApi/flubu.exe");
-            var deployScript = Path.GetFullPath("Updates/WebApi/DeployScript.cs");
+            Thread.Sleep(2000);
+            
+            string frameworkName = Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()
+                ?.FrameworkName;
+            var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            bool isNetCore = frameworkName.StartsWith(".NETCoreApp");
+            string flubuPath, deployScript;
+            if (isWindows)
+            {
+              
+               flubuPath = Path.GetFullPath("Updates/WebApi/flubu.exe");
+               if (isNetCore)
+               {
+                   deployScript = Path.GetFullPath("Updates/WebApi/DeployScript.cs");
+               }
+               else
+               {
+                   deployScript = Path.GetFullPath("Updates/WebApi/DeploymentScript.cs");
+               }
+
+             
+            }
+            else
+            {
+                flubuPath = Path.GetFullPath("Updates/WebApi/Deploy.bat");
+                deployScript = Path.GetFullPath("Updates/WebApi/DeploymentScript.cs");
+                var processRestore = Process.Start(new ProcessStartInfo
+                {
+                    WorkingDirectory = Path.GetDirectoryName(flubuPath),
+                    FileName = "dotnet restore",
+                    Arguments = "Deploy.csproj",
+                });
+
+                processRestore.WaitForExit();
+            }
+
             Console.WriteLine($"path: {deployScript}");
             var process = Process.Start(new ProcessStartInfo
             {
                 WorkingDirectory = Path.GetDirectoryName(flubuPath),
-                FileName = flubuPath,
+                FileName = "dotnet flubu",
                 Arguments = $"-s={deployScript}"
             });
          
