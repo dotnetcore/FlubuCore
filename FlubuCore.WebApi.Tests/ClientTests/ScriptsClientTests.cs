5b46679fda6d7e422c91aa5197daa013e30cdaa1
markoz <mzorec@gmail.com>
2019-03-26 23:33:32 +0100

    Updates tests.

--- FlubuCore.WebApi.Tests/ClientTests/ScriptsClientTests.cs
+++ FlubuCore.WebApi.Tests/ClientTests/ScriptsClientTests.cs
@@ -57,7 +57,7 @@ namespace FlubuCore.WebApi.Tests.ClientTests
             };
             req.ScriptArguments.Add("FileName", "test.txt");
             var response = await Client.ExecuteScriptAsync(req);
-            Assert.Equal(7, response.Logs.Count);
+            Assert.Equal(8, response.Logs.Count);
             Assert.StartsWith("Executing action method \"FlubuCore.WebApi.Controllers.ScriptsController.Execute", response.Logs[0]);
             Assert.Equal("SuccesfullTarget finished (took 0 seconds)", response.Logs[response.Logs.Count - 3]);
             Assert.True(File.Exists("test.txt"));
@@ -195,7 +195,7 @@ namespace FlubuCore.WebApi.Tests.ClientTests
             Assert.Equal(HttpStatusCode.InternalServerError, exception.StatusCode);
             Assert.Equal(ErrorCodes.InternalServerError, exception.ErrorCode);
             Assert.Equal("Error message", exception.ErrorMessage);
-            Assert.Equal(8, exception.Logs.Count);
+            Assert.Equal(9, exception.Logs.Count);
             Assert.StartsWith("Executing action method \"FlubuCore.WebApi.Controllers.ScriptsController.Execute", exception.Logs[0]);
             Assert.StartsWith("Exception occured:", exception.Logs[exception.Logs.Count - 1]);
         }
