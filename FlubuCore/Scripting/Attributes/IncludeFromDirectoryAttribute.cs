69ddf85f6b5fb18add5753322a504274961a262b
markoz <mzorec@gmail.com>
2019-03-28 09:06:12 +0100

    Fixes type

--- FlubuCore/Scripting/Attributes/IncludeFromDirectoryAttribute.cs
+++ FlubuCore/Scripting/Attributes/IncludeFromDirectoryAttribute.cs
@@ -8,14 +8,14 @@ namespace FlubuCore.Scripting.Attributes
     /// Include all other c# source code file(.cs) to script from specified directory.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-    public class IncludeFromDirectroyAttribute : Attribute
+    public class IncludeFromDirectoryAttribute : Attribute
     {
         /// <summary>
         ///  Include all other c# source code file(.cs) to script from specified directory.
         /// </summary>
         /// <param name="directory">Directory path.</param>
         /// <param name="subDirectories">Include directory sub directories.</param>
-        public IncludeFromDirectroyAttribute(string directory, bool subDirectories = false)
+        public IncludeFromDirectoryAttribute(string directory, bool subDirectories = false)
         {
         }
     }
