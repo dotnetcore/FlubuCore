9fd89aa4001eaf719cdfed2d7e8e9a11a724a759
markoz <mzorec@gmail.com>
2019-04-11 23:47:39 +0200

    Adds back old way script compilation for .net 462

--- FlubuCore/Scripting/ScriptLoader.cs
+++ FlubuCore/Scripting/ScriptLoader.cs
@@ -88,6 +88,7 @@ namespace FlubuCore.Scripting
                 .CreateBuildScriptInstanceOldWayAttribute);
 
 #if NET462
+          oldWay = true;
 #endif
             var references = GetBuildScriptReferences(args, projectFileAnalyzerResult, scriptAnalyzerResult, code,
                 oldWay, buildScriptFilePath);
