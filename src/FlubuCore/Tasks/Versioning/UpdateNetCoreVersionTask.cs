98f6394fe015a4f84f2e7dfec9b6006539409d5b
Marko Zorec <mzorec@gmail.com>
2019-06-18 08:19:19 +0200

    Option to update version quality (suffix)

--- src/FlubuCore/Tasks/Versioning/UpdateNetCoreVersionTask.cs
+++ src/FlubuCore/Tasks/Versioning/UpdateNetCoreVersionTask.cs
@@ -14,7 +14,7 @@ namespace FlubuCore.Tasks.Versioning
 
         private readonly IFileWrapper _file;
 
-        private Version _version;
+        private BuildVersion _version;
         private string _description;
 
         public UpdateNetCoreVersionTask(IPathWrapper pathWrapper, IFileWrapper filWrapper, string file)
@@ -46,7 +46,20 @@ namespace FlubuCore.Tasks.Versioning
         /// </summary>
         /// <param name="version"></param>
         /// <returns></returns>
-        public UpdateNetCoreVersionTask FixedVersion(Version version)
+        [Obsolete("Use set version instead")]
+        public UpdateNetCoreVersionTask FixedVersion(BuildVersion version)
+        {
+            _version = version;
+            return this;
+        }
+
+        /// <summary>
+        /// Use fixed version instead of fetching one from <see cref="IBuildPropertiesSession"/> build property named: <see cref="BuildProps.BuildVersion"/>
+        /// </summary>
+        /// <param name="version"></param>
+        /// <returns></returns>
+        [Obsolete]
+        public UpdateNetCoreVersionTask SetVersion(BuildVersion version)
         {
             _version = version;
             return this;
@@ -81,7 +94,9 @@ namespace FlubuCore.Tasks.Versioning
         {
             if (_version == null)
             {
-                _version = context.Properties.GetBuildVersion();
+                _version = new BuildVersion();
+                _version.Version = context.Properties.GetBuildVersion();
+                _version.VersionQuality = context.Properties.GetBuildVersionQuality();
             }
 
             if (_version == null)
@@ -90,7 +105,7 @@ namespace FlubuCore.Tasks.Versioning
             }
 
             DoLogInfo($"Update version to {_version}");
-            string newVersion = _version.ToString(3);
+            string newVersion = _version.Version.ToString(3);
             int res = 0;
 
             foreach (string file in _files)
@@ -106,13 +121,23 @@ namespace FlubuCore.Tasks.Versioning
                     return 1;
                 }
 
+                string newVersionWithQuality = newVersion;
+                if (!string.IsNullOrEmpty(_version.VersionQuality))
+                {
+                    if (!_version.VersionQuality.StartsWith("-"))
+                    {
+                        _version.VersionQuality = _version.VersionQuality.Insert(0, "-");
+                        newVersionWithQuality = $"{newVersionWithQuality}{_version.VersionQuality}";
+                    }
+                }
+
                 if (_pathWrapper.GetExtension(file).Equals(".xproj", StringComparison.CurrentCultureIgnoreCase))
                 {
                     UpdateJsonFileTask task = context.Tasks().UpdateJsonFileTask(file);
 
                     task
                         .FailIfPropertyNotFound(false)
-                        .Update("version", newVersion);
+                        .Update("version", newVersionWithQuality);
 
                     AdditionalProperties.ForEach(i => task.Update(i, newVersion));
 
@@ -121,8 +146,8 @@ namespace FlubuCore.Tasks.Versioning
                 else
                 {
                     var task = context.Tasks().UpdateXmlFileTask(file);
-                    task.AddOrUpdate("Project/PropertyGroup/Version", newVersion);
-                    newVersion = _version.ToString(4);
+                    task.AddOrUpdate("Project/PropertyGroup/Version", newVersionWithQuality);
+                    newVersion = _version.Version.ToString(4);
                     task.AddOrUpdate("Project/PropertyGroup/AssemblyVersion", newVersion);
                     task.AddOrUpdate("Project/PropertyGroup/FileVersion", newVersion);
                     task.Execute(context);
