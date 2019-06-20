3fd8f692df458bdade173c861f000420ccee12e0
Marko Zorec <mzorec@gmail.com>
2019-06-20 13:08:32 +0200

    Fixes GitVersion deserialization

--- src/FlubuCore/Tasks/Versioning/GitVersion.cs
+++ src/FlubuCore/Tasks/Versioning/GitVersion.cs
@@ -6,56 +6,56 @@ namespace FlubuCore.Tasks.Versioning
 {
     public class GitVersion
     {
-        public virtual int Major { get; internal set; }
+        public virtual int Major { get; set; }
 
-        public virtual int Minor { get; internal set; }
+        public virtual int Minor { get; set; }
 
-        public virtual int Patch { get; internal set; }
+        public virtual int Patch { get; set; }
 
-        public virtual string PreReleaseTag { get; internal set; }
+        public virtual string PreReleaseTag { get; set; }
 
-        public virtual string PreReleaseTagWithDash { get; internal set; }
+        public virtual string PreReleaseTagWithDash { get; set; }
 
-        public virtual string PreReleaseLabel { get; internal set; }
+        public virtual string PreReleaseLabel { get; set; }
 
-        public virtual string PreReleaseNumber { get; internal set; }
+        public virtual string PreReleaseNumber { get; set; }
 
-        public virtual string BuildMetaData { get; internal set; }
+        public virtual string BuildMetaData { get; set; }
 
-        public virtual string BuildMetaDataPadded { get; internal set; }
+        public virtual string BuildMetaDataPadded { get; set; }
 
-        public virtual string FullBuildMetaData { get; internal set; }
+        public virtual string FullBuildMetaData { get; set; }
 
-        public virtual string MajorMinorPatch { get; internal set; }
+        public virtual string MajorMinorPatch { get; set; }
 
-        public virtual string SemVer { get; internal set; }
+        public virtual string SemVer { get; set; }
 
-        public virtual string LegacySemVer { get; internal set; }
+        public virtual string LegacySemVer { get; set; }
 
-        public virtual string LegacySemVerPadded { get; internal set; }
+        public virtual string LegacySemVerPadded { get; set; }
 
-        public virtual string AssemblySemVer { get; internal set; }
+        public virtual string AssemblySemVer { get; set; }
 
-        public virtual string FullSemVer { get; internal set; }
+        public virtual string FullSemVer { get; set; }
 
-        public virtual string InformationalVersion { get; internal set; }
+        public virtual string InformationalVersion { get; set; }
 
-        public virtual string BranchName { get; internal set; }
+        public virtual string BranchName { get; set; }
 
-        public virtual string Sha { get; internal set; }
+        public virtual string Sha { get; set; }
 
-        public virtual string NuGetVersionV2 { get; internal set; }
+        public virtual string NuGetVersionV2 { get; set; }
 
-        public virtual string NuGetVersion { get; internal set; }
+        public virtual string NuGetVersion { get; set; }
 
-        public virtual string NuGetPreReleaseTagV2 { get; internal set; }
+        public virtual string NuGetPreReleaseTagV2 { get; set; }
 
-        public virtual string NuGetPreReleaseTag { get; internal set; }
+        public virtual string NuGetPreReleaseTag { get; set; }
 
-        public virtual string CommitsSinceVersionSource { get; internal set; }
+        public virtual string CommitsSinceVersionSource { get; set; }
 
-        public virtual string CommitsSinceVersionSourcePadded { get; internal set; }
+        public virtual string CommitsSinceVersionSourcePadded { get; set; }
 
-        public virtual string CommitDate { get; internal set; }
+        public virtual string CommitDate { get; set; }
     }
 }
