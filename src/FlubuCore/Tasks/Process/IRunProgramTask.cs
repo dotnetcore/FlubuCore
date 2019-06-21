7cdd3f91174b4a64c2a9c80337853824a0aa21d1
Marko Zorec <mzorec@gmail.com>
2019-06-21 08:01:02 +0200

    Adds WithArgument to IRunProgramTask interface

--- src/FlubuCore/Tasks/Process/IRunProgramTask.cs
+++ src/FlubuCore/Tasks/Process/IRunProgramTask.cs
@@ -7,6 +7,21 @@ namespace FlubuCore.Tasks.Process
     public interface IRunProgramTask : ITaskOfT<int, IRunProgramTask>, IExternalProcess<IRunProgramTask>
     {
         /// <summary>
+        /// Add argument for executable.
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <param name="maskArg">If <c>true</c> argument is masked. Otherwise not.</param>
+        /// <returns></returns>
+        new IRunProgramTask WithArguments(string arg, bool maskArg);
+
+        /// <summary>
+        /// Add arguments for executable.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        new IRunProgramTask WithArguments(params string[] args);
+
+        /// <summary>
         ///     Capture output of the running program.
         /// </summary>
         /// <returns></returns>
