e410df8c6d75ce09b7a2893707e4f604dce43eb3
Marko Zorec <mzorec@gmail.com>
2019-06-26 09:49:42 +0200

    fixes hints for flubu options

--- src/FlubuCore/Scripting/DefaultBuildScript.cs
+++ src/FlubuCore/Scripting/DefaultBuildScript.cs
@@ -155,9 +155,9 @@ namespace FlubuCore.Scripting
                 runInTerminalMode = true;
                 var source = new Dictionary<char, IReadOnlyCollection<string>>();
                 var propertyKeys = ScriptProperties.GetPropertiesKeys(this, taskSession);
-                propertyKeys.Add("--parallel");
-                propertyKeys.Add("--dryrun");
-                propertyKeys.Add("--noColor");
+                propertyKeys.Add("-parallel");
+                propertyKeys.Add("-dryrun");
+                propertyKeys.Add("-noColor");
                 source.Add('-', propertyKeys);
 
                 inputReader = new ConsoleHintedInput(taskSession.TargetTree.GetTargetNames().ToList(), source);
