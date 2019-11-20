336868904bcc42c6900fd02dae34b0e4b6e24add
Marko Zorec <mzorec@gmail.com>
2019-11-20 10:19:50 +0100

    adds GetFiles, GetDirectory, GetVsSolution, GetRootDirectory extensions to IBuildPropertyContext

--- src/FlubuCore/Context/Extensions/ContextBaseExtensions.cs
+++ src/FlubuCore/Context/Extensions/ContextBaseExtensions.cs
@@ -1,69 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading;
 using FlubuCore.IO;
 using FlubuCore.Tasks.Solution.VSSolutionBrowsing;
 using GlobExpressions;
 
 namespace FlubuCore.Context
 {
-    public static class TaskContextExtensions
+    public static class ContextBaseExtensions
     {
-        public static string GetEnvironmentVariable(this ITaskContext context, string variable)
-        {
-            return Environment.GetEnvironmentVariable(variable);
-        }
-
-        public static void WaitForDebugger(this ITaskContext context)
-        {
-            context.LogInfo("Waiting for debugger to be attached.");
-
-            while (!Debugger.IsAttached)
-            {
-                Thread.Sleep(100);
-            }
-
-            context.LogInfo("Debugger attached.");
-        }
-
         /// <summary>
         /// Get's Visual studio solution information. if <see cref="solutionFileName"/> is not specified solution file name is readed from <see cref="IBuildPropertiesContext"/> property <see cref="BuildProps.SolutionFileName"/>
         /// </summary>
         /// <param name="context"></param>
         /// <param name="solutionFileName"></param>
         /// <returns></returns>
-        public static VSSolution GetVsSolution(this ITaskContext context, string solutionFileName = null)
+        public static VSSolution GetVsSolution(ITaskContext context, string solutionFileName = null)
         {
-            return ContextBaseExtensions.GetVsSolution(context, solutionFileName);
-        }
+            VSSolution solution = null;
+            bool saveSolution = true;
+            if (string.IsNullOrEmpty(solutionFileName))
+            {
+                solutionFileName = context.Properties.TryGet<string>(BuildProps.SolutionFileName);
+                solution = context.Properties.TryGet<VSSolution>(BuildProps.Solution);
+            }
+            else
+            {
+                saveSolution = false;
+            }
 
-        /// <summary>
-        /// Get's product root directory.
-        /// </summary>
-        public static FullPath RootDirectory(this ITaskContext context)
-        {
-            return new FullPath(context.Properties.GetProductRootDir());
-        }
+            if (solution == null)
+            {
+                solution = context.Tasks().LoadSolutionTask(solutionFileName).DoNotFailOnError().Execute(context);
+            }
 
-        public static FileFullPath OutputDirectory(this ITaskContext context)
-        {
-            return context.RootDirectory().AddFileName(context.Properties.GetOutputDir());
-        }
+            if (saveSolution)
+            {
+                context.Properties.Set(BuildProps.Solution, solution);
+            }
 
-        /// <summary>
-        /// Gets all files matching glob pattern.
-        /// See: https://github.com/kthompson/glob for supported pattern expressions and use cases.
-        /// </summary>
-        /// <param name="context"></param>
-        /// <param name="directory"></param>
-        /// <param name="globPattern"></param>
-        /// <returns></returns>
-        public static List<FileFullPath> GetFiles(this ITaskContext context, string directory, params string[] globPattern)
-        {
-           return GetFiles(context, directory, GlobOptions.None, globPattern);
+            return solution;
         }
 
         /// <summary>
@@ -74,12 +50,25 @@ namespace FlubuCore.Context
         /// <param name="directory"></param>
         /// <param name="globPattern"></param>
         /// <returns></returns>
-        public static List<FileFullPath> GetFiles(this ITaskContext context, string directory, GlobOptions globOptions = GlobOptions.None, params string[] globPattern)
+        public static List<FileFullPath> GetFiles(string directory, params string[] globPattern)
+        {
+            return GetFiles(directory, GlobOptions.None, globPattern);
+        }
+
+        /// <summary>
+        /// Gets all files matching glob pattern.
+        /// See: https://github.com/kthompson/glob for supported pattern expressions and use cases.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="directory"></param>
+        /// <param name="globPattern"></param>
+        /// <returns></returns>
+        public static List<FileFullPath> GetFiles(string directory, GlobOptions globOptions = GlobOptions.None, params string[] globPattern)
         {
             var directoryInfo = new DirectoryInfo(directory);
             return globPattern.SelectMany(pattern => Glob.Files(directoryInfo, pattern, globOptions)).Select(x => new FileFullPath(x.FullName)).ToList();
         }
-
+        
         /// <summary>
         /// Gets all directories matching glob pattern.
         /// </summary>
@@ -87,9 +76,9 @@ namespace FlubuCore.Context
         /// <param name="directory"></param>
         /// <param name="globPattern"></param>
         /// <returns></returns>
-        public static List<FullPath> GetDirectories(this ITaskContext context, string directory, params string[] globPattern)
+        public static List<FullPath> GetDirectories(string directory, params string[] globPattern)
         {
-            return GetDirectories(context, directory, GlobOptions.None, globPattern);
+            return GetDirectories(directory, GlobOptions.None, globPattern);
         }
 
         /// <summary>
@@ -99,20 +88,10 @@ namespace FlubuCore.Context
         /// <param name="directory"></param>
         /// <param name="globPattern"></param>
         /// <returns></returns>
-        public static List<FullPath> GetDirectories(this ITaskContext context, string directory, GlobOptions globOptions = GlobOptions.None, params string[] globPattern)
+        public static List<FullPath> GetDirectories(string directory, GlobOptions globOptions = GlobOptions.None, params string[] globPattern)
         {
             var directoryInfo = new DirectoryInfo(directory);
             return globPattern.SelectMany(pattern => Glob.Directories(directoryInfo, pattern, globOptions)).Select(x => new FullPath(x.FullName)).ToList();
-        }
-
-        /// <summary>
-        /// Local git repository information.
-        /// </summary>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public static Git Git(this ITaskContext context)
-        {
-            return new Git(context);
         }
     }
 }
