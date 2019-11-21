215826335e48811389b664089ece7cf5c98783c0
Marko Zorec <mzorec@gmail.com>
2019-11-21 07:11:25 +0100

    Fixes issue with duplicated extension methods

--- src/FlubuCore/Context/Extensions/TaskContextExtensions.cs
+++ src/FlubuCore/Context/Extensions/TaskContextExtensions.cs
@@ -29,80 +29,9 @@ namespace FlubuCore.Context
             context.LogInfo("Debugger attached.");
         }
 
-        /// <summary>
-        /// Get's Visual studio solution information. if <see cref="solutionFileName"/> is not specified solution file name is readed from <see cref="IBuildPropertiesContext"/> property <see cref="BuildProps.SolutionFileName"/>
-        /// </summary>
-        /// <param name="context"></param>
-        /// <param name="solutionFileName"></param>
-        /// <returns></returns>
-        public static VSSolution GetVsSolution(this ITaskContext context, string solutionFileName = null)
-        {
-            return ContextBaseExtensions.GetVsSolution(context, solutionFileName);
-        }
-
-        /// <summary>
-        /// Get's product root directory.
-        /// </summary>
-        public static FullPath RootDirectory(this ITaskContext context)
-        {
-            return new FullPath(context.Properties.GetProductRootDir());
-        }
-
         public static FileFullPath OutputDirectory(this ITaskContext context)
         {
             return context.RootDirectory().AddFileName(context.Properties.GetOutputDir());
-        }
-
-        /// <summary>
-        /// Gets all files matching glob pattern.
-        /// See: https://github.com/kthompson/glob for supported pattern expressions and use cases.
-        /// </summary>
-        /// <param name="context"></param>
-        /// <param name="directory"></param>
-        /// <param name="globPattern"></param>
-        /// <returns></returns>
-        public static List<FileFullPath> GetFiles(this ITaskContext context, string directory, params string[] globPattern)
-        {
-           return GetFiles(context, directory, GlobOptions.None, globPattern);
-        }
-
-        /// <summary>
-        /// Gets all files matching glob pattern.
-        /// See: https://github.com/kthompson/glob for supported pattern expressions and use cases.
-        /// </summary>
-        /// <param name="context"></param>
-        /// <param name="directory"></param>
-        /// <param name="globPattern"></param>
-        /// <returns></returns>
-        public static List<FileFullPath> GetFiles(this ITaskContext context, string directory, GlobOptions globOptions = GlobOptions.None, params string[] globPattern)
-        {
-            var directoryInfo = new DirectoryInfo(directory);
-            return globPattern.SelectMany(pattern => Glob.Files(directoryInfo, pattern, globOptions)).Select(x => new FileFullPath(x.FullName)).ToList();
-        }
-
-        /// <summary>
-        /// Gets all directories matching glob pattern.
-        /// </summary>
-        /// <param name="context"></param>
-        /// <param name="directory"></param>
-        /// <param name="globPattern"></param>
-        /// <returns></returns>
-        public static List<FullPath> GetDirectories(this ITaskContext context, string directory, params string[] globPattern)
-        {
-            return GetDirectories(context, directory, GlobOptions.None, globPattern);
-        }
-
-        /// <summary>
-        /// Gets all directories matching glob pattern.
-        /// </summary>
-        /// <param name="context"></param>
-        /// <param name="directory"></param>
-        /// <param name="globPattern"></param>
-        /// <returns></returns>
-        public static List<FullPath> GetDirectories(this ITaskContext context, string directory, GlobOptions globOptions = GlobOptions.None, params string[] globPattern)
-        {
-            var directoryInfo = new DirectoryInfo(directory);
-            return globPattern.SelectMany(pattern => Glob.Directories(directoryInfo, pattern, globOptions)).Select(x => new FullPath(x.FullName)).ToList();
         }
 
         /// <summary>
