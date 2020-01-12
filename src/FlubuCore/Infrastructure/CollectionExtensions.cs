60f139eb42f6a554a802c06e5d24f9a37477bb5c
Marko Zorec <mzorec@gmail.com>
2020-01-12 07:08:52 +0100

    Migrates From Microsoft.DotnetCli

---
+++ src/FlubuCore/Infrastructure/CollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlubuCore.Infrastructure
+{
+    public static class CollectionExtensions
+    {
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+        {
+            foreach (T obj in items)
+                collection.Add(obj);
+        }
+    }
+}
