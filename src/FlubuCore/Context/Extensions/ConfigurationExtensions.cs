e3cbf164c25c14b7bcd383697ec1f78eb4ead77b
Marko Zorec <mzorec@gmail.com>
2020-02-15 19:26:42 +0100

    Adds Replace extension method to IServiceCollection.

---
+++ src/FlubuCore/Context/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlubuCore.Context
+{
+    public static class ConfigurationExtensions
+    {
+        public static IServiceCollection Replace<TService, TImplementation>(
+            this IServiceCollection services,
+            ServiceLifetime lifetime)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            var descriptorToRemove = services.FirstOrDefault(d => d.ServiceType == typeof(TService));
+
+            services.Remove(descriptorToRemove);
+
+            var descriptorToAdd = new ServiceDescriptor(typeof(TService), typeof(TImplementation), lifetime);
+
+            services.Add(descriptorToAdd);
+
+            return services;
+        }
+    }
+}
