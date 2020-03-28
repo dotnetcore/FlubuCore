2860c2cbc83779d103d964e6b393683b5f9d1a2a
Marko Zorec <mzorec@gmail.com>
2020-03-28 01:48:18 +0100

    Adds OutputDir attrbiute

---
+++ src/FlubuCore/Context/Attributes/BuildProperties/OutputDirAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlubuCore.Context.Attributes.BuildProperties
+{
+    public class OutputDirAttribute : BuildPropertyAttribute
+    {
+        public OutputDirAttribute()
+            : base(BuildProps.BuildDir)
+        {
+        }
+    }
+}
