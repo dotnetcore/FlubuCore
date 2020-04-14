fe17b39c0bcedf79226a1db4c646e880c6f9ef4c
Marko Zorec <mzorec@gmail.com>
2020-04-14 13:22:33 +0200

    Adds ConsoleTestApp

---
+++ src/FlubuCore.ConsoleTestApp/DefaultTestScript.cs
@@ -0,0 +1,17 @@
+﻿using FlubuCore.Context;
+using FlubuCore.Context.Attributes;
+using FlubuCore.Scripting;
+
+namespace FlubuCore.ConsoleTestApp
+{
+    public class TestScript : DefaultBuildScript
+    {
+        [BuildProperty(DotNetBuildProps.OutputDir)] 
+        public string Output{ get; set; } = "Output222";
+
+        protected override void ConfigureTargets(ITaskContext context)
+        {
+            context.CreateTarget("Test").SetAsDefault().AddCoreTask(x => x.Build());
+        }
+    }
+}
