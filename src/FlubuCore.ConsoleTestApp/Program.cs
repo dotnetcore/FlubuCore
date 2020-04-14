7cb3071172fd8b9343edfb38d1be74cd5d46dc00
Marko Zorec <mzorec@gmail.com>
2020-04-14 13:22:33 +0200

    no message

---
+++ src/FlubuCore.ConsoleTestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using FlubuCore.Scripting;
+
+namespace FlubuCore.ConsoleTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            args = new[] {"Test"};
+
+            var engine = new FlubuEngine();
+            engine.RunScript<BuildScript>(args);
+        }
+    }
+}
