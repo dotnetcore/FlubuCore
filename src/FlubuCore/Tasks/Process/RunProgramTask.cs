2f0e9e47b4b1c1993a5085adafab90d9d68f5194
Michael Tsai <huanlin.tsai@gmail.com>
2020-05-19 10:47:52 +0800

    Simplify log output.

--- src/FlubuCore/Tasks/Process/RunProgramTask.cs
+++ src/FlubuCore/Tasks/Process/RunProgramTask.cs
@@ -249,15 +249,7 @@ namespace FlubuCore.Tasks.Process
             }
 
             DoLogInfo($"Running program from '{workingFolder}':");
-
-            if (command.CommandName != null && command.CommandName.Contains("cmd.exe"))
-            {
-                DoLogInfo($"{command.CommandName} {cmd}{commandArgs}{Environment.NewLine}", Color.DarkCyan);
-            }
-            else
-            {
-                DoLogInfo($"{command.CommandName}{commandArgs}{Environment.NewLine}", Color.DarkCyan);
-            }
+            DoLogInfo($"{cmd}{commandArgs}{Environment.NewLine}", Color.DarkCyan);
 
             int res = command.Execute()
                 .ExitCode;
