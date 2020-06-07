8569e0bc9097ee7e1a4f2ba3860e5c1e5ddfd30e
Michael Tsai <huanlin.tsai@gmail.com>
2020-06-08 03:06:48 +0800

    Show working folder with full path (#326)
    * Show working folder with full path.
    
    * Full path for log output only.

--- src/FlubuCore.ConsoleTestApp/DefaultTestScript.cs
+++ src/FlubuCore.ConsoleTestApp/DefaultTestScript.cs
@@ -1,16 +1,22 @@
 ﻿using FlubuCore.Context;
 using FlubuCore.Context.Attributes;
+using FlubuCore.Context.Attributes.BuildProperties;
 using FlubuCore.Scripting;
 
 namespace FlubuCore.ConsoleTestApp
 {
     public class DefaultTestScript : DefaultBuildScript
-    {        
+    {
+        [SolutionFileName]
+        public string SolutionFileName => "flubu.sln";
+
         public string Output => RootDirectory.CombineWith("output222");
         
         protected override void ConfigureTargets(ITaskContext context)
         {
-            context.CreateTarget("Test").SetAsDefault().AddCoreTask(x => x.Build());
+            context.CreateTarget("Test")
+                .SetAsDefault()
+                .AddCoreTask(x => x.Restore().WorkingFolder("../../../.."));
         }
     }
 }
